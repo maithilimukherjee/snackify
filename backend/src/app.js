7336529b21436d0ab2b2fdb046807285b2787d8f
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-12-31 15:08:08 +0530

    add updated url to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ console.log("app.js loaded");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://snackify-three.vercel.app",
   credentials: true,
 }));
 
