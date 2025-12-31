3df4434152d081eedd371fe4f0400cd96b929971
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-12-31 16:38:38 +0530

    remove error

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, verify2FA, auth } from "../controllers/authController.js";
+import { register, login, auth } from "../controllers/authController.js";
 import { pool } from "../config/db.js";
 
 const router = express.Router();
