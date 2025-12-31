57ae1c464e90ddbcc64a903bf88fc49105186ccc
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-12-31 15:56:53 +0530

    replace ethereal mail with gmailsmtp

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -4,8 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  service: "gmail",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
