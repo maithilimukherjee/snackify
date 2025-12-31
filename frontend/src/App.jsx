5789744dec145e39d21526c42c038c160cd4a323
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-12-31 16:34:27 +0530

    fix App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/register";
-import Verify2FA from "./pages/verify2fa";
 import Dashboard from "./pages/dashboard";
 import Profile from "./pages/profile";
 import Recommend from "./pages/recommend";
@@ -13,7 +12,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/verify2fa" element={<Verify2FA />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/recommendRecipes" element={<Recommend />} />
